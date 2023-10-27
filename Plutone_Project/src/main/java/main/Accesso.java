3d50b37389ca80597e82ca3f26c6b62ce7379ddf
Tanjiro-Natsu <m.palazzo36@studenti.uniba.it>
2023-10-27 17:18:20 +0200

    Add existing file

---
+++ Plutone_Project/src/main/java/main/Accesso.java
@@ -0,0 +1,59 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
+
+public class Accesso extends JFrame {
+	
+	
+	static private  String a=null;
+	static private String q1=null;
+	static private String q2=null;
+	
+	
+	
+	public static String getjdbc() {
+		return "jdbc:sqlserver://localhost:1433;databaseName=Plutone_Project";
+	}
+	public static String getUser() {
+		return "sa";
+	}
+	public static String getPassword() {
+		return "Cacca1989";
+	}
+
+	
+
+	
+	
+	
+	
+	
+
+}
---
+++ Plutone_Project/src/main/java/main/Info.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Info  extends JFrame{
+	 Info(){
+		 setSize(300,100);
+		 this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+		 
+	
+
+}
+}
